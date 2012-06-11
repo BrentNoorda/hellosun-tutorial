a23afff25ac3d919878d1e445a56f5005bdb9cd7
Brent Noorda <brent.noorda@gmail.com>
2012-06-11 14:20:01 -0700

    * heroku command now requests these engines * use new css to make bubble quotes * make it look better on an iphone * don't send message if the username or message are blank * always scroll to bottom of the page * lint * add links to our the tutorial pages

--- public/javascripts/chatty.js
+++ public/javascripts/chatty.js
@@ -1,5 +1,5 @@
 /*jslint white:false plusplus:false nomen:false */
-/*globals $, io */
+/*globals $, io, window, document */
 
 var socket = io.connect();
 
@@ -10,11 +10,22 @@ function simple_html_escape(s)
 
 function appendMsg(msg) {
 	$('#msgs').append(function() {
-		var div = $('<div>');
-		div.html('<b>' + simple_html_escape(msg.username) + ':</b> ' + simple_html_escape(msg.message));
+		var div, html;
+		div = $('<div>');
+		html = '<div class="bubble-content">' +
+					simple_html_escape(msg.message) +
+				'</div>\r\n' +
+				'<div class="quote-tail"></div>\r\n' +
+					'<div class="quote-tail2"></div>\r\n' +
+					'<div class="whom">' +
+						simple_html_escape(msg.username) +
+					'</div>\r\n' +
+				'</div>\r\n';
+		div.html(html);
 		return div;
 	});
-	$('#msgs')[0].scrollTop = $('#msgs')[0].scrollHeight;
+	//$('#msgs')[0].scrollTop = $('#msgs')[0].scrollHeight;
+	window.scrollTo(0, document.body.scrollHeight);
 }
 
 socket.on('msg', function(data) {
@@ -29,12 +40,19 @@ socket.on('init', function(data) {
 	});
 });
 
+function trim(stringToTrim) {
+	return stringToTrim.replace(/^\s+|\s+$/g,"");
+}
+
 function sendMsg() {
 	var msg = {};
 	$.each($('#chat').serializeArray(), function(i,v) {
-		msg[v.name] = v.value;
+		msg[v.name] = trim(v.value);
 	});
-	$("#msg").val("");
-	appendMsg(msg);
-	socket.emit('msg', JSON.stringify(msg));
+	if ( msg.message.length && msg.username.length )
+	{
+		$("#msg").val("");
+		appendMsg(msg);
+		socket.emit('msg', JSON.stringify(msg));
+	}
 }
